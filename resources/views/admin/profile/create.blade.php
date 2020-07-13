aefc1e5f7c6353780c479e4f4b084c4dbd5c7877
yuka yamada <yu-40@outlook.jp>
2020-07-13 14:24:27 +0000

    fourth commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"    
-    content="IE=edge">
-        <meta name="viewport" content="width=device-width, 
-    initial-scale=1">
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-         <title>Profile</title>
-    </head>
-    <body>
-         <h1>Profile作成画面</h1>
-    </body>
-</heml>
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'MY プロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MY プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
