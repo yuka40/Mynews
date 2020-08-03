145b72e727b9ea92dcaec3a96b1d793eb3339b69
yuka yamada <yu-40@outlook.jp>
2020-08-03 12:12:45 +0000

    10 commmit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>MYプロフィール</h2>
-                <form action="{{ action('Admin\PlofileController@update') }}" method="post" enctype="multipart/form-data">
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -17,31 +17,31 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="title">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ $news_form->title }}">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->neme }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="title">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ $news_form->title }}">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="title">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ $news_form->title }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="body">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ $news_form->body }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction}}</textarea>
                         </div>
                     </div>
                    
                     <div class="form-group row">
                         <div class="col-md-10">
-                            <input type="hidden" name="id" value="{{ $news_form->id }}">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="更新">
                         </div>
